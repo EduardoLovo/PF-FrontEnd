6109e72508001bb14aedce44673ee66d087430dc
Eduardolovo <eduardo.llovo@gmail.com>
2021-09-15 23:48:12 -0300

    iamgem

--- src/components/structure/TarefaCard/TarefaCard.js
+++ src/components/structure/TarefaCard/TarefaCard.js
@@ -10,7 +10,7 @@ const TarefaCard = (props) => {
     <Link to={`/view/${tarefa._id}`} className="card">
       <div>
         <div className="card-img">
-          <img src={"https://warsaw.com.br/wp-content/uploads/2020/09/teste.png"} alt="" />
+          <img src={"https://store-images.s-microsoft.com/image/apps.64291.9007199266483650.4f907d9a-c60f-4272-8000-be3bf737f363.47c91d60-546d-43ba-b713-671493401c50?mode=scale&q=90&h=300&w=300"} alt="" />
         </div>
         <p className="card-text">{tarefa.Titulo}</p>
         <span className="card-info">{tarefa.Descuricao}</span>
